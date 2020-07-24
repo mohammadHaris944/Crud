088cd5d0c4884bffbfb5e107aec2890a2f07dad3
mohammadHaris944 <mohammad.haris@devbox.co>
2020-07-24 13:50:41 +0500

    Add User Profile Info

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,12 @@
 class UsersController < ApplicationController
-    before_action :set_user, only: [:edit,:update]
+    before_action :set_user, only: [:edit,:update,:show]
     def new
+        @user=User.new
     end
     def edit
+    end
+    def show
+        @articles=@user.articles
     end
     def update
         if(@user.update(user_params))
@@ -12,7 +16,6 @@ class UsersController < ApplicationController
             render 'edit'
         end
     end
-
     def create
         @user=User.new(user_params)
         if(@user.save)
