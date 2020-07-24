56cf47808df3a77226a292631f7f152eb43fcf48
mohammadHaris944 <mohammad.haris@devbox.co>
2020-07-24 12:18:37 +0500

    Add user edit and update Action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,18 @@
 class UsersController < ApplicationController
+    before_action :set_user, only: [:edit,:update]
     def new
-        @user=User.new
     end
+    def edit
+    end
+    def update
+        if(@user.update(user_params))
+            flash[:notice]="User updated successfully"
+            redirect_to articles_path
+        else
+            render 'edit'
+        end
+    end
+
     def create
         @user=User.new(user_params)
         if(@user.save)
@@ -15,5 +26,8 @@ class UsersController < ApplicationController
     def user_params
         params.require(:user).permit(:username,:email,:password)
 
+    end
+    def set_user
+        @user=User.find(params[:id])
     end
 end
